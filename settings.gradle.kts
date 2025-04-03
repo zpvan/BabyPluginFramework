e9600a553ab3a6e48204c52f60ae9f15b3214e64
knox <843762968@qq.com>
2025-04-03 08:12:15 +0800

    (code) init, add aliyun maven, use a pretty .gitignore

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    repositories {
+        // 添加阿里云 maven 地址
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        // 添加阿里云 maven 地址
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BabyPluginFramework"
+include(":app")
