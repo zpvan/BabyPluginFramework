d0f5d03035441a56a910ec87b3c74427ace9a45b
knox <843762968@qq.com>
2025-04-08 23:20:14 +0800

    (build) PluginApk slimming, 45MB -> 29MB

--- pluginapk/build.gradle.kts
+++ pluginapk/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(project(":pluginlibrary"))
+    compileOnly(project(":pluginlibrary"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
